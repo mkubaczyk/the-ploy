189ec0f3b45ba58aa6bd2d9958587cc9b98f8172
Mateusz Kubaczyk <mkubaczyk@apptension.com>
2018-10-22 15:08:14 +0200

    Change default logger msg format

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"github.com/adjust/rmq"
-	logger "github.com/apsdehal/go-logger"
+	"github.com/apsdehal/go-logger"
 	"os"
 )
 
@@ -18,7 +18,8 @@ var TaskQueue rmq.Queue
 var Logger *logger.Logger
 
 func Init() {
-	Logger, _ = logger.New("stdout", 1, os.Stdout)
+	logger.SetDefaultFormat("%{time:2006-01-02 15:04:05} [%{level}] %{message}")
+	Logger, _ = logger.New("logger", 1, os.Stdout)
 	Logger.Info("Initializing config")
 	redisAddr := getEnv("REDIS_ADDR", "127.0.0.1:6379")
 	RedisConn = rmq.OpenConnection("Queue", "tcp", redisAddr, 0)
